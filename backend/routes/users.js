dbe1e10820998c45584cdeca1c1af3b6e3bdee6d
Harwin14 <agungharwinn@gmail.com>
2022-12-25 21:00:22 +0700

    crud

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,150 +4,168 @@ var models = require('../models')
 var { Response } = require('../helpers/util')
 const { Op } = require('sequelize')
 
-/* GET users listing. */
-router.get('/', async (req, res, next) => {
-  try {
-    const { page, name, phone } = req.query
-    const limit = 6
-    const offset = (page - 1) * limit
-    const total = await models.User.count()
-    const pages = Math.ceil(total / limit)
+
+// router.get('/', async (req, res, next) => {
+//   try {
+
+//     const {name } = req.query
+    
+//     const users = await models.User.findAll({order: [
+//                 ['id', 'ASC']
+//             ]})
+//     res.json(new Response(users))
+//   }
+//   catch (error) {
+//     console.log(error)
+//     res.status(500).json(new Response(error, false))
+//   }
+// })
+
+  /* GET users listing. */
+  router.get('/', async (req, res, next) => {
+    try {
+      const { page, name, phone } = req.query
+      const limit = 6
+      const offset = (page - 1) * limit
+      const total = await models.User.count()
+      const pages = Math.ceil(total / limit)
 
 
-    if (name && phone) {
-      const users = await models.User.findAll({
-        limit,
-        offset,
-        where: {
-          [Op.or]: [
-            {
-              name: {
-                [Op.iLike]: `%${name}%`
+      if (name && phone) {
+        const users = await models.User.findAll({
+          limit,
+          offset,
+          where: {
+            [Op.or]: [
+              {
+                name: {
+                  [Op.iLike]: `%${name}%`
+                }
+              },
+              {
+                phone: {
+                  [Op.iLike]: `%${phone}%`
+                }
               }
-            },
-            {
-              phone: {
-                [Op.iLike]: `%${phone}%`
-              }
-            }
+            ]
+          },
+          order: [
+            ['id', 'ASC']
           ]
-        },
-        order: [
-          ['id', 'ASC']
-        ]
-      })
+        })
 
-      res.json(new Response({
-        users,
-        page: Number(page),
-        pages
-      }))
+        res.json(new Response({
+          users,
+          page: Number(page),
+          pages
+        }))
 
-    } else if (name) {
-      const users = await models.User.findAll({
-        limit,
-        offset,
-        where: {
-          [Op.and]: [
-            {
-              name: {
-                [Op.iLike]: `%${name}%`
+      } else if (name) {
+        const users = await models.User.findAll({
+          limit,
+          offset,
+          where: {
+            [Op.and]: [
+              {
+                name: {
+                  [Op.iLike]: `%${name}%`
+                }
               }
-            }
+            ]
+          },
+          order: [
+            ['id', 'ASC']
           ]
-        },
-        order: [
-          ['id', 'ASC']
-        ]
-      })
+        })
 
-     
-      res.json(new Response({
-        users,
-        page: Number(page),
-        pages
-      }))
-    } else if (phone) {
-      const users = await models.User.findAll({
-        limit,
-        offset,
-        where: {
-          [Op.and]: [
-            {
-              phone: {
-                [Op.iLike]: `%${phone}%`
+
+        res.json(new Response({
+          users,
+          page: Number(page),
+          pages
+        }))
+      } else if (phone) {
+        const users = await models.User.findAll({
+          limit,
+          offset,
+          where: {
+            [Op.and]: [
+              {
+                phone: {
+                  [Op.iLike]: `%${phone}%`
+                }
               }
-            }
+            ]
+          },
+          order: [
+            ['id', 'ASC']
           ]
-        },
-        order: [
-          ['id', 'ASC']
-        ]
-      })
-
-      res.json(new Response({
-        users,
-        page: Number(page),
-        pages
-      }))
-
-    } else {
-      const users = await models.User.findAll({
-        limit,
-        offset,
-        order: [
-          ['id', 'ASC']
-        ]
-      })
-
+        })  
   
-      res.json(new Response({
-        users,
-        page: Number(page),
-        pages
-      }))
-    }
-  } catch (error) {
-    console.log(error)
-    res.status(500).json(new Response(error, false))
-  }
-});
+        res.json(new Response({
+          users,
+          page: Number(page),
+          pages
+        }))
 
-router.post('/', async function (req, res, next) {
-  try {
-    const user = await models.User.create(req.body)
-    res.json(new Response(user))
-  } catch (err) {
-    res.status(500).json(new Response(err, false))
-  }
-});
+      } else {
+        const users = await models.User.findAll({
+          limit,
+          offset,
+          order: [
+            ['id', 'ASC']
+          ]
+        })
 
-router.put('/:id', async function (req, res, next) {
-  try {
-    const user = await models.User.update(req.body, {
-      where: {
-        id: req.params.id
-      },
-      returning: true,
-      plain: true
-    })
-    res.json(new Response(user[1]))
-  } catch (err) {
-    res.status(500).json(new Response(err, false))
-  }
-});
 
-router.delete('/:id', async function (req, res, next) {
-  try {
-    const user = await models.User.destroy({
-      where: {
-        id: req.params.id
+        res.json(new Response({
+          users,
+          page: Number(page),
+          pages
+        }))
       }
-    })
-    res.json(new Response(user))
-  } catch (err) {
-    res.status(500).json(new Response(err, false))
-  } 
-}); 
- 
-module.exports = router;
+    } catch (error) {
+      console.log(error)
+      res.status(500).json(new Response(error, false))
+    }
+  });
+
+
+  router.post('/', async function (req, res, next) {
+    try {
+      const user = await models.User.create(req.body)
+      res.json(new Response(user))
+    } catch (err) {
+      res.status(500).json(new Response(err, false))
+    }
+  });
+
+  router.put('/:id', async function (req, res, next) {
+    try {
+      const user = await models.User.update(req.body, {
+        where: {
+          id: req.params.id
+        },
+        returning: true,
+        plain: true
+      })
+      res.json(new Response(user[1]))
+    } catch (err) {
+      res.status(500).json(new Response(err, false))
+    }
+  });
+
+  router.delete('/:id', async function (req, res, next) {
+    try {
+      const user = await models.User.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+      res.json(new Response(user))
+    } catch (err) {
+      res.status(500).json(new Response(err, false))
+    }
+  });
+
+  module.exports = router;
